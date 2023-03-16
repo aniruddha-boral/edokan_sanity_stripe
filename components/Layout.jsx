927af29b8579399677ef5916bf0ddea2a695c855
redmi8adual11121314 <redmi8adual11121314@gmail.com>
2023-03-16 16:33:34 +0530

    inr

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>E-Shop </title>
+        <title>E-Shop</title>
       </Head>
       <header>
         <Navbar />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Online Shopping Platform 2</Link>
+        <Link href="/">Online Shopping Platform sdrge</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>

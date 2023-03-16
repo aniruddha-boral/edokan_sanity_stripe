54adc5df9754a4b8686260b398cff0985222fb63
redmi8adual11121314 <redmi8adual11121314@gmail.com>
2023-03-16 14:59:56 +0530

    inr

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: 'usd',
+              currency: 'inr',
               product_data: { 
                 name: item.name,
                 images: [newImage],

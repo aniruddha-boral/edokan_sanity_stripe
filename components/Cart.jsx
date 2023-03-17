f2ac298b73f988fb365d2560a2ae2d9133971d11
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-17 16:41:34 +0530

    e

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -67,6 +67,10 @@ const Cart = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
+                 
+                </div>
+                <div className="flex top">
+                  
                   <h4>₹{item.price}</h4>
                 </div>
                 <div className="flex bottom">

817235cbcf218459f9649a32fb5216c7033286ab
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-17 17:11:16 +0530

    dd

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -99,15 +99,12 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              
-            </div>
-            <div className="total">
-              
               <h3>₹{totalPrice}</h3>
             </div>
+            
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
-                Pay with Stripe
+              Proceed to Payment
               </button>
             </div>
           </div>

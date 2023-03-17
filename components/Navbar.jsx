1eb56e482272625ba56585a9e675332f0e31d89f
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-17 16:05:03 +0530

    d

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Online Shopping Platform 45v3</Link>
+        <Link href="/">Online Shopping Platform</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>

06fb285bb4f0968e01c3fb60089a09641a71f3b0
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-17 23:12:15 +0530

    rr

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    /*<HeroBanner heroBanner={bannerData.length && bannerData[0]}  />*/
+    
     <div className="products-heading">
       <h2>Best of Electronics</h2>
       <p>Curated for You</p>

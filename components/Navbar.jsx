621152617103f8fbf51f321c7897251f2dec5a36
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-17 22:12:06 +0530

    ffr

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
       <p className="logo">
         <Link href="/">Online Shopping Platform</Link>
       </p>
-      <p className="sanity-icon" >
-      <AiOutlineShopping href="/"><AiOutlineShopping />
-      </p>
+      <button className="sanity-icon" >
+        <AiOutlineShopping />
+      </button>
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
        <span className="cart-item-qty">{totalQuantities}</span>

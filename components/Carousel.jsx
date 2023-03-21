fb43b7bdb496ed1736340c07139c6d6434328b00
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-21 18:05:11 +0530

    ffggg

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
 
 
+var React = require('react');
 var ReactDOM = require('react-dom');
 var Carousel = require('react-responsive-carousel').Carousel;
 

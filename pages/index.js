f9751d51bbf9a1e9cad0c34778e8149935fc2e22
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-21 18:11:08 +0530

    fef

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 import { client } from '../lib/client';
-import { Product, FooterBanner, HeroBanner, Carousel } from '../components';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <Carousel />
+    <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
+                <div>
+                    <img src="/aura.jpg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="/aura_2.jpg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+              </Carousel>
     <div className="products-heading">
       <h2>Best of Electronics</h2>
       <p>Curated for You</p>

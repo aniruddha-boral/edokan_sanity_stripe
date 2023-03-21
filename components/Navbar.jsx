cce484d27adee54bf94eafabb508c991deef5860
aniruddha boral <redmi8adual11121314@gmail.com>
2023-03-21 16:10:32 +0530

    ERE

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping, AiOutlineDatabase } from 'react-icons/ai'
-
 import { Cart } from './';
 import { useStateContext} from '../context/StateContext';
 
@@ -10,35 +9,22 @@ const Navbar = () => {
 
   return (
     <div className="navbar-container">
-      <a>
+      <a href="/">
         <img class="img-responsive" src="/favicon.ico"/>
        </a>
-      
-      <p className="logo2">
-        <Link href="/">U</Link>
+      <p className="logo">ULU
       </p>
-      <p className="logo3">
-        <Link href="/">L</Link>
-      </p>
-      <p className="logo4">
-        <Link href="/">U</Link>
-      </p>
-
       <Link href="https://aniruddhaboral2.sanity.studio/desk">
           <button type="button" className="sanity-icon">
           <AiOutlineDatabase />
           </button>
         </Link>
-
-     
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-
       {showCart && <Cart />}
     </div>
   )
 }
-
 export default Navbar

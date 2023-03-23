8583497b87c1459660cd3b9b74ff8b0c934e1a62
redmi8adual11121314 <redmi8adual11121314@gmail.com>
2023-03-23 18:07:30 +0530

    t44t4

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = ({ products, bannerData }) => (
   <div>
-   
+    <Carousel>
+                <div>
+                    <img src="aura.jpg" />
+                   
+                </div>
+                <div>
+                    <img src="aura_2.jpg" />
+                    
+                </div>
+                <div>
+                    <img src="aura_3.jpg" />
+                    
+                </div>
+            </Carousel>
+   <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best of Electronics</h2>
       <p>Curated for You</p>
@@ -15,7 +30,7 @@ const Home = ({ products, bannerData }) => (
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
-    
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 

5aa7481917f8ded9708903ca5e878a852ed595ad
redmi8adual11121314 <redmi8adual11121314@gmail.com>
2023-06-19 12:13:24 +0530

    wdwd

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
      
       <p className="navtitle">GULU
       </p>
-       <button type="button" className="user-icon">
+       <button type="button" className="user-icon" href="/">
           <AiOutlineSmile />
        </button>
         <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>

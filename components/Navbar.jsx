881c3495ae8d5bf916731983fd8249e11a35c945
redmi8adual11121314 <redmi8adual11121314@gmail.com>
2023-06-19 13:42:58 +0530

    sdvfsdf

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
      
-      <p className="navtitle">TURBO</p>
+      <p className="navtitle">Gucci</p>
       <a className="user-icon" href="https://aniruddha-boral.netlify.app/">
           <AiOutlineUser />
        </a>
